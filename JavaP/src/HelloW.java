67e6ae3bb39452edfbcd2a4251f75733c3b6cd7a
peekay32 <five11.1988@gmail.com>
2017-05-13 17:33:10 +0530

    Comment 5

--- JavaP/src/HelloW.java
+++ JavaP/src/HelloW.java
@@ -2,8 +2,9 @@
 public class HelloW {
 	public static void main(String[] arg)
 	{
-		//Changed 	comment 3
-		System.out.println("Hello All");
+		//Changed 	comment 5
+		for(int i=5;i>0;i--)
+			System.out.println("Hello All");
 	}
 
 }
